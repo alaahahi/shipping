091425d91634a6d21489cb698b899101cd94fb06
alaahahi <alaa.hahe@gmail.com>
2023-10-08 23:29:14 +0300

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+    <style>
+      @page  
+    { 
+        size: auto;   /* auto is the initial value */ 
+    
+        /* this affects the margin in the printer settings */ 
+        margin: 0;  
+        margin-top: 60px
+    } 
+    </style>
 </head>
 <body style="direction: rtl;">
 <div class="container">       
@@ -122,7 +131,7 @@
     $(document).ready(function() {
         // Function to open the print dialog
         function openPrintDialog() {
-           // window.print();
+            window.print();
         }
     
         // Call the function to open the print dialog
