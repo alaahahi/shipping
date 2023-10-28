07e9d9b10b2f25f76d2020c45e650e609d7ad26a
alaahahi <alaa.hahe@gmail.com>
2023-10-29 01:36:09 +0300

    aasd

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    build: {
-        outDir: 'public', // Set the output directory to the 'public' folder
-      },
+
     plugins: [
         laravel({
             input: 'resources/js/app.js',
