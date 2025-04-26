f42078bd6d5057d4fce4dd94f02d94e075df609f
ALAA HAHI <alaa.hahe@gmail.com>
2025-04-26 16:13:42 +0300

    Revert "add"
    This reverts commit 6743e649720e342708b4bea3fddc4b339c857f11.

--- resources/views/receiptPayment.blade.php
+++ resources/views/receiptPayment.blade.php
@@ -118,7 +118,7 @@ $Help = new MyHelp();
 
     <div class="col-6 ps-5 text-start">
        Mobile:
-       0750 4440 927
+    0770 445 9964
     </div>
   </div>
   {{-- <div class="row text-center py-2">
@@ -257,7 +257,7 @@ $Help = new MyHelp();
 
       <div class="col-6 ps-5 text-start">
          Mobile:
-         0750 4440 927
+      0770 445 9964
       </div>
     </div>
     {{-- <div class="row text-center py-2">
