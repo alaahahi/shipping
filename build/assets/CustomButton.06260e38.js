ba10b4a8da5630d47804e3ef37608bc85ecd5be7
alaahahi <alaa.hahe@gmail.com>
2025-08-02 18:34:19 +0300

    aa

--- build/assets/CustomButton.06260e38.js
+++
@@ -1,1 +0,0 @@
-import{a,o as l}from"./app.44831d8a.js";const i={__name:"CustomButton",props:["rowIndex","model","save","close"],setup(n){const{rowIndex:r,model:s,save:u,close:e}=n,c=t=>{const o=new CustomEvent("cell",{bubbles:!0,detail:{row:s}});$el.dispatchEvent(o),t.stopPropagation(),typeof e=="function"&&e()};return(t,o)=>(l(),a("button",{onClick:c},"Edit"))}};export{i as default};
