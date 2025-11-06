b5c2cd927412c43380f5197fa35ab427ddbe7fe5
alaahahi <alaa.hahe@gmail.com>
2025-11-06 16:29:35 +0300

    Update AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -92,7 +92,7 @@ const switchLocale = (locale) => {
               </div>
               
               <!-- صفحة البحث Offline -->
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"  v-if="$page.props.auth.user.type_id==1||$page.props.auth.user.type_id==6 &&false">
+              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"  v-if="false">
                 <NavLink
                   :href="route('offline.car.search')"
                   :active="route().current('offline.car.search')"
@@ -101,7 +101,7 @@ const switchLocale = (locale) => {
                 </NavLink>
               </div>
               
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"  v-if="$page.props.auth.user.type_id==1||$page.props.auth.user.type_id==6 && false">
+              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"  v-if="false">
                 <NavLink
                   :href="route('online_contracts')"
                   :active="route().current('online_contracts')"
