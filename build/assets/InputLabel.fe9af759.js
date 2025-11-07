442399f4e819a623e0fd35a70f13cc7ac9476384
alaahahi <alaa.hahe@gmail.com>
2025-11-07 05:10:42 +0300

    add

---
+++ build/assets/InputLabel.fe9af759.js
@@ -0,0 +1,1 @@
+import{o as e,a as t,t as o,d as n}from"./app.c6c6bb61.js";const l={class:"block font-medium text-sm text-gray-700 dark:text-gray-400"},r={key:0},c={key:1},d={__name:"InputLabel",props:["value"],setup(a){return(s,_)=>(e(),t("label",l,[a.value?(e(),t("span",r,o(a.value),1)):(e(),t("span",c,[n(s.$slots,"default")]))]))}};export{d as _};
